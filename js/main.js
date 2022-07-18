7938b4319d7074580416c730f7142808a7e2ba7f
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-07-18 13:19:55 +0200

    inserito dati team dentro array

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const team =    [
+                    {
+                        nome: "Wayne Barnett",
+                        ruolo: "Founder & CEO",
+                        "immagine profilo": "wayne-barnett-founder-ceo.jpg"
+                    },
+                    {
+                        nome: "Angela Caroll",
+                        ruolo: "Chief Editor",
+                        "immagine profilo": "angela-caroll-chief-editor.jpg"
+                    },
+                    {
+                        nome: "Walter Gordon",
+                        ruolo: "Office Manager",
+                        "immagine profilo": "walter-gordon-office-manager.jpg"
+                    },
+                    {
+                        nome: "Angela Lopez",
+                        ruolo: "Social Media Manager",
+                        "immagine profilo": "angela-lopez-social-media-manager.jpg"
+                    },
+                    {
+                        nome: "Scott Estrada",
+                        ruolo: "Developer",
+                        "immagine profilo": "scott-estrada-developer.jpg"
+                    },
+                    {
+                        nome: "Barbara Ramos",
+                        ruolo: "Graphic Designer",
+                        "immagine profilo": "barbara-ramos-graphic-designer.jpg"
+                    }
+                ]
+
+console.log(team);
